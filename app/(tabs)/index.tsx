4c37c44c085d4ddce7cabeb3f9520d3fabec5502
Tanuv Nair <tanuvnair@gmail.com>
2025-05-05 19:44:55 +0530

    Completed app

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,11 @@
+import domtoimage from "dom-to-image";
 import { type ImageSource } from "expo-image";
 import * as ImagePicker from "expo-image-picker";
-import React, { useRef, useState } from "react";
-import { StyleSheet, View } from "react-native";
+import * as MediaLibrary from "expo-media-library";
+import { useRef, useState } from "react";
+import { Platform, StyleSheet, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { captureRef } from "react-native-view-shot";
 
 import Button from "@/components/Button";
 import CircleButton from "@/components/CircleButton";
@@ -10,15 +14,10 @@ import EmojiPicker from "@/components/EmojiPicker";
 import EmojiSticker from "@/components/EmojiSticker";
 import IconButton from "@/components/IconButton";
 import ImageViewer from "@/components/ImageViewer";
-import * as MediaLibrary from "expo-media-library";
-import { captureRef } from "react-native-view-shot";
-
-import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
-    const [status, requestPermission] = MediaLibrary.usePermissions();
     const [selectedImage, setSelectedImage] = useState<string | undefined>(
         undefined
     );
@@ -27,11 +26,13 @@ export default function Index() {
     const [pickedEmoji, setPickedEmoji] = useState<ImageSource | undefined>(
         undefined
     );
+    const [status, requestPermission] = MediaLibrary.usePermissions();
     const imageRef = useRef<View>(null);
 
     if (status === null) {
         requestPermission();
     }
+
     const pickImageAsync = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ["images"],
@@ -60,19 +61,35 @@ export default function Index() {
     };
 
     const onSaveImageAsync = async () => {
-        // we will implement this later
-        try {
-            const localUri = await captureRef(imageRef, {
-                height: 440,
-                quality: 1,
-            });
+        if (Platform.OS !== "web") {
+            try {
+                const localUri = await captureRef(imageRef, {
+                    height: 440,
+                    quality: 1,
+                });
 
-            await MediaLibrary.saveToLibraryAsync(localUri);
-            if (localUri) {
-                alert("Saved!");
+                await MediaLibrary.saveToLibraryAsync(localUri);
+                if (localUri) {
+                    alert("Saved!");
+                }
+            } catch (e) {
+                console.log(e);
             }
-        } catch (e) {
-            console.log(e);
+        } else {
+            try {
+                const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+                    quality: 0.95,
+                    width: 320,
+                    height: 440,
+                });
+
+                let link = document.createElement("a");
+                link.download = "sticker-smash.jpeg";
+                link.href = dataUrl;
+                link.click();
+            } catch (e) {
+                console.log(e);
+            }
         }
     };
 
